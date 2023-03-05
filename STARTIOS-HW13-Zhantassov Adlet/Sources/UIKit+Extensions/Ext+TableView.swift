551dc5429c526fd231eb3f4ad51f5e65181033fe
Adlet <adlet.zh19966@gmail.com>
2023-03-05 17:49:07 +0600

    add tableview/delegate/datasource/structure of project

---
+++ STARTIOS-HW13-Zhantassov Adlet/Sources/UIKit+Extensions/Ext+TableView.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  STARTIOS-HW13-Zhantassov Adlet
+//
+//  Created by Adlet Zhantassov on 05.03.2023.
+//
+
+import Foundation
+import UIKit
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+}
