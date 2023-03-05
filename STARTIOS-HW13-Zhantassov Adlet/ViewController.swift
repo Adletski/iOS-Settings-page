0f7c6591e103c2e8f3416e76024543198c6cc505
Adlet <adlet.zh19966@gmail.com>
2023-03-05 17:06:18 +0600

    Initial Commit

---
+++ STARTIOS-HW13-Zhantassov Adlet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  STARTIOS-HW13-Zhantassov Adlet
+//
+//  Created by Adlet Zhantassov on 05.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
